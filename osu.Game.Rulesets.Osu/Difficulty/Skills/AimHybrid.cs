de1bff442f866b4c1f30a23ad1dc84183d2cbfa6
Apollo-P <45495521+Apollo-P@users.noreply.github.com>
2020-10-30 21:00:24 +0000

    hybrid aimmmm

--- osu.Game.Rulesets.Osu/Difficulty/Skills/AimHybrid.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/AimHybrid.cs
@@ -16,55 +16,82 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
     public class AimHybrid : OsuSkill
     {
         private double StrainDecay = 0.25;
-        protected override double SkillMultiplier => 8000;
+        private const float prevMultiplier = 0.33f;
+        protected override double SkillMultiplier => 2500;
         protected override double StrainDecayBase => StrainDecay;
-        protected override double StarMultiplierPerRepeat => 1.1;
-        private const double distThresh = 150;
+        protected override double StarMultiplierPerRepeat => 1.07;
 
         protected override double StrainValueOf(DifficultyHitObject current)
         {
-            StrainDecay = 0.25;
+            StrainDecay = .25;
 
             if (current.BaseObject is Spinner)
                 return 0;
 
-            var osuCurrent = (OsuDifficultyHitObject)current;
-            if (osuCurrent.BaseObject is Slider && osuCurrent.TravelTime < osuCurrent.StrainTime) StrainDecay = Math.Min(osuCurrent.TravelTime, osuCurrent.StrainTime - 30.0) / osuCurrent.StrainTime *
-                (1.0 - Math.Pow(1.0 - StrainDecay, Math.Pow(1.0 + osuCurrent.TravelDistance / Math.Max(osuCurrent.TravelTime, 30.0), 3.0))) +
-                Math.Max(30.0, osuCurrent.StrainTime - osuCurrent.TravelTime) / osuCurrent.StrainTime * StrainDecay;
-
-            test.Add(Tuple.Create(current.BaseObject.StartTime, 0.0));
-
             double strain = 0;
-            double velScale = 0;
-            double sliderVel = 1.0 + osuCurrent.TravelDistance / osuCurrent.TravelTime;
 
-            if (Previous.Count > 0 && osuCurrent.Angle != null)
+            if (Previous.Count > 1)
             {
-                var osuPrevious = (OsuDifficultyHitObject)Previous[0];
-                double awkVal = 0;
+                // though we are on current, we want to use current as reference for previous,
+                // so for what its worth, difficulty calculation is a function of Previous[0]
+                var osuPrevObj = (OsuDifficultyHitObject)Previous[1];
+                var osuCurrentObj = (OsuDifficultyHitObject)Previous[0];
+                var osuNextObj = (OsuDifficultyHitObject)current;
 
-                double maxTime = Math.Max(osuCurrent.StrainTime, osuPrevious.StrainTime);
-                double minTime = Math.Min(osuCurrent.StrainTime, osuPrevious.StrainTime);
+                // Here we set a custom strain decay rate that decays based on # of objects rather than MS.
+                // This is just so we can focus on balancing only the strain rewarded, and time no longer matters.
+                // this will make a repeated pattern "cap out" or reach 85 maximum difficulty in 12 objects.
+                StrainDecay = Math.Pow(.9, 1000.0 / Math.Min(osuCurrentObj.StrainTime, 200.0));
 
-                double currDistance = osuCurrent.JumpDistance + osuCurrent.TravelDistance;
-                double prevDistance = osuPrevious.JumpDistance + osuPrevious.TravelDistance;
+                // here we generate a value of being snappy or flowy that is fed into the gauss error function to build a probability.
+                var x = (osuCurrentObj.JumpDistance - (Math.Pow(Math.Sin(Math.Min(osuNextObj.JumpDistance, Math.PI / 2)), 2)
+                        * (osuCurrentObj.JumpDistance / 3)
+                        * Math.Pow(Math.Sin((double)osuCurrentObj.Angle), 2)))
+                        * (osuCurrentObj.DeltaTime - 50);
 
-                double currVel = currDistance / osuCurrent.StrainTime;
-                double prevVel = prevDistance / osuPrevious.StrainTime;
+                // this is where we use an ERF function to derive a probability.
+                var flowProb = 0.5 - 0.5 * erf((-75 + x) / (25 * Math.Sqrt(2)));
+                var snapProb = 0.5 + 0.5 * erf((-75 + x) / (25 * Math.Sqrt(2)));
 
-                double diffDist = Math.Abs(currDistance - prevDistance);
-                double maxDist = Math.Max(Math.Max(currDistance, prevDistance), distThresh);
-                double minDist = Math.Max(Math.Min(currDistance, prevDistance), distThresh);
+                // Create velocity vectors, scale prior by prevMultiplier
+                var prevVector = Vector2.Multiply(Vector2.Divide(osuPrevObj.DistanceVector, (float)osuPrevObj.StrainTime), prevMultiplier);
+                var currVector = Vector2.Divide(osuCurrentObj.DistanceVector, (float)osuCurrentObj.StrainTime);
 
-                velScale = Math.Min(currVel, prevVel);
+                // add them to get our final velocity, length is the observed velocity and thus the difficulty.
+                var flowVelocity = Vector2.Subtract(currVector, prevVector).Length;
+                var snapVelocity = Vector2.Add(currVector, prevVector).Length;
 
-                awkVal = diffDist / maxDist;
-                strain = awkVal / maxTime;
+                var snapStrain = snapVelocity * Math.Max(0.5, snapProb);
+                var flowStrain = flowVelocity * Math.Max(0.5, flowProb);
 
-                test.Add(Tuple.Create(current.BaseObject.StartTime, awkVal));
+                strain = snapStrain + flowStrain;
             }
-            return strain * velScale * sliderVel;
+
+            return strain;
+
+        }
+        private static double erf(double x)
+        {
+            // constants
+            double a1 = 0.254829592;
+            double a2 = -0.284496736;
+            double a3 = 1.421413741;
+            double a4 = -1.453152027;
+            double a5 = 1.061405429;
+            double p = 0.3275911;
+
+            // Save the sign of x
+            int sign = 1;
+            if (x < 0)
+                sign = -1;
+            x = Math.Abs(x);
+
+            // A&S formula 7.1.26
+            double t = 1.0 / (1.0 + p * x);
+            double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.Exp(-x * x);
+
+            return sign * y;
         }
     }
+
 }
